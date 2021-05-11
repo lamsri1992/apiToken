593457cd7f55af7f249e911d4a15f74fbe4b561e
lamsri1992 <satiya3577@gmail.com>
2021-05-11 13:58:54 +0700

    build api cm

---
+++ app/Http/Controllers/api/labController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Response;
+use DB;
+
+
+class labController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['Hello, CMH']);
+        // ip/api/labs/ => หน้า index ของ api/labs
+    }
+
+    public function show($id)
+    {
+        $result = DB::table('covid_copy2')
+                ->select('cid', 'fname', 'address', 'tel')
+                ->where('cid', $id)
+                ->first();
+        return Response::json([
+                    '_data' => $result
+                ], 200);
+        // ถ้าไม่เอา _data ให้ลบเหลือแค่ $result
+        // status code 200
+        // ip/api/labs/เลข 13 หลัก
+    }
+}
